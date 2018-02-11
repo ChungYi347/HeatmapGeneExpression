c6bee03b659bdca28012954422c473eaa558727f
Chunggi Lee Local <asdzxc6442@unist.ac.kr>
2018-02-11 12:46:46 +0900

    Menu

--- static/javascripts/ui.js
+++ static/javascripts/ui.js
@@ -4,3 +4,14 @@ $(document).ready(function () {
         $('body').toggleClass('config-closed');
     });
 });
+
+$(function () {
+    $("#param-dropdown").on('click', 'li a', function () {
+        $("#param-toggle").text($(this).text());
+        $("#param-toggle").val($(this).text());
+    });
+    $("#color-dropdown li a").click(function () {
+        $("#color-toggle").text($(this).text());
+        $("#color-toggle").val($(this).text());
+    });
+})
