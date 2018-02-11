4f1dde158a517f5ba996b2487b9a5365c75739aa
Chunggi Lee Local <asdzxc6442@unist.ac.kr>
2018-02-11 17:24:46 +0900

    making dendrogram ..

--- clustering/clusters.py
+++ clustering/clusters.py
@@ -1,4 +1,5 @@
-import sys, numpy, scipy, csv, pandas
+import sys, scipy, csv, pandas
+import numpy as np
 import scipy.cluster.hierarchy as hier
 import scipy.spatial.distance as dist
 import scipy.stats as stats
@@ -18,37 +19,55 @@ class HierarchyCluster(object):
             return True
         return False
 
-    def cluster(self):
-        data_matrix, row_headers, col_headers = self.data['data_matrix'], self.data['row_headers'], self.data['col_headers']
-
+    def transform(self, data_matrix):
         # Detect whether log2 transform and tranform data using log2
-        express_value = pandas.DataFrame(numpy.log2(data_matrix))
+        express_value = pandas.DataFrame(np.log2(data_matrix))
         express_value = express_value.fillna(0)
         qunant_val = express_value.quantile([0., 0.25, 0.5, 0.75, 0.99, 1.0])
         avg_quan_val = (qunant_val[0] + qunant_val[1] + qunant_val[2] + qunant_val[3]) / 4
         if self.chk_log2(avg_quan_val):
             # log2 transform
-            data_matrix = numpy.log2(data_matrix)
-            # convert native data array into a numpy array
-            # col_data_matrix = numpy.log2(col_data_matrix)
+            data_matrix = np.log2(data_matrix)
+
+        #col_data_matrix = data_matrix.transpose()
 
         # Zscore transform in order to make normal distribution
         data_matrix = stats.zscore(data_matrix, 1, 1)
-        #col_data_matrix = stats.zscore(col_data_matrix, 1, 1)
+        return data_matrix
+
+    def make_dendrogram(self, node, dendrogram):
+        dendrogram.append(node.get_id())
+        dendrogram = self.make_dendrogram(node.get_left(), dendrogram)
+        dendrogram = self.make_dendrogram(node.get_right(), dendrogram)
+        return dendrogram
+
+    def cal_linkage(self, data_matrix, row_headers = None):
+        req = {
+            "ordered_data_matrix" : [],
+            "ordered_row_headers" : []
+        }
 
         # calculate distance matrix
         distance_matrix = dist.pdist(data_matrix)
         distance_square_matrix = dist.squareform(distance_matrix)
         linkage_matrix = hier.linkage(distance_square_matrix)
 
-        heatmap_order = hier.leaves_list(linkage_matrix)
-        ordered_data_matrix = data_matrix[heatmap_order, :]
+        rootnode, nodelist = hier.to_tree(linkage_matrix, rd=True)
+        print(self.make_dendrogram(rootnode, []))
 
-        row_headers = numpy.array(row_headers)
-        ordered_row_headers = row_headers[heatmap_order]
+        heatmap_order = hier.leaves_list(linkage_matrix)
+        req["ordered_data_matrix"] = data_matrix[heatmap_order, :]
+
+        if row_headers:
+            row_headers = np.array(row_headers)
+            req["ordered_row_headers"] = row_headers[heatmap_order]
+
+        return req
+
+    def save_result(self, row_link, col_link):
         matrix_output = []
         row = 0
-        for row_data in ordered_data_matrix:
+        for row_data in row_link['ordered_data_matrix']:
             col = 0
             row_output = []
             for col_data in row_data:
@@ -56,12 +75,27 @@ class HierarchyCluster(object):
                 col += 1
             matrix_output.append(row_output)
             row += 1
+        return matrix_output
 
-        print('var maxData = ' + str(numpy.amax(data_matrix)) + ";")
-        print('var minData = ' + str(numpy.amin(data_matrix)) + ";")
-        print('var data = ' + str(matrix_output) + ";")
-        print('var cols = ' + str(col_headers) + ";")
-        print('var rows = ' + str([x for x in ordered_row_headers]) + ";")
+    def cluster(self):
+        data_matrix, row_headers, col_headers = self.data['data_matrix'], self.data['row_headers'], self.data['col_headers']
+
+        data_matrix = self.transform(data_matrix)
+        col_data_matrix = self.transform(data_matrix.transpose())
+
+        row = self.cal_linkage(data_matrix)
+        col = self.cal_linkage(col_data_matrix)
+
+        min_val = np.amin(data_matrix)
+        max_val = np.amax(data_matrix)
+
+        #print(self.save_result(row, col))
+
+        #print('var maxData = ' + str(np.amax(data_matrix)) + ";")
+        #print('var minData = ' + str(np.amin(data_matrix)) + ";")
+        #print('var data = ' + str(matrix_output) + ";")
+        #print('var cols = ' + str(col_headers) + ";")
+        #print('var rows = ' + str([x for x in ordered_row_headers]) + ";")
 
 
 def clustering(input_path=None, output_path=None, param="single", axis="both", delimiter="csv"):
@@ -91,7 +125,7 @@ def clustering(input_path=None, output_path=None, param="single", axis="both", d
                 data_matrix.append([float(x) for x in data[1:]])
 
             # transform list to numpy array
-            data_matrix = numpy.array(data_matrix)
+            data_matrix = np.array(data_matrix)
 
             data = {
                 "data_matrix" : data_matrix,
